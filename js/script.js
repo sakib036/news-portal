3a720092ae2dc1e13d03f8a8c08ca7334f989da5
Md. Nazmus Sakib <sakib036@gmail.com>
2022-09-02 23:06:59 +0600

    menu-item

--- js/script.js
+++ js/script.js
@@ -1,4 +1,33 @@
 // menu section added 
-const menuList = () =>{
-    const menuUrl=``
+const menuList =async () =>{
+    const menuUrl=`https://openapi.programming-hero.com/api/news/categories`;
+    const res=await fetch(menuUrl);
+    const data=await res.json();
+    return (data.data.news_category);
 }
+const displayMenuList= async() =>{
+    const menuItems=await menuList();
+    const menuSection=document.getElementById('menu-section');
+    
+    menuItems.forEach(menu => {
+        // console.log(menu);
+        const menuLi=document.createElement('li');
+        menuLi.innerHTML=`<a onclick="menuId('${menu.category_id}')">${menu.category_name}</a>`;
+        menuSection.appendChild(menuLi);
+    });
+   
+
+}
+const menuId=(id)=>{
+    console.log(id)
+    const categoryItem=async()=>{
+        const categoryUrl=`https://openapi.programming-hero.com/api/news/category/${id}`;
+    const res=await fetch(categoryUrl);
+    const categories=await res.json();
+    
+    console.log(categories);
+   }
+   categoryItem();
+}
+
+displayMenuList();
