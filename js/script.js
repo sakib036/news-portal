36cfe9999b1136ead5224494f24a7e67bdeb8200
Md. Nazmus Sakib <sakib036@gmail.com>
2022-09-03 05:01:25 +0600

    card-view-serial

--- js/script.js
+++ js/script.js
@@ -37,24 +37,36 @@ const menuId=(id, categoryName)=>{
     categoryFound.classList.remove('hidden');
     categoryP.innerText=`${datas.length? datas.length:"No"} items Found for category of ${name}`
     categoryFound.appendChild(categoryP)
-    // console.log(datas,name)
-    datas.forEach(data=>{
+    const valus=datas.sort((a, b) => b.total_view - a.total_view);
+    valus.forEach(data=>{
         const{image_url,title,details,author,rating,total_view}=data;
+       
         const cardDiv=document.createElement('div');
         cardDiv.innerHTML=`
-        <div class="card md:card-side bg-base-100 shadow-xl my-4">
-        <figure  class="w-full md:w-1/3"><img class="w-full h-full" src="${image_url}" alt="Album"></figure>
-        <div class="card-body w=2/3">
-          <h2 class="card-title">'${title? title:"NO Title Found"}'</h2>
-          <p>${details.length > 200 ? details.slice(0,200) + '...' : details}</p>
-          <div class="card-actions justify-end">
-            <button class="btn btn-primary">Listen</button>
-          </div>
-        </div>
+        <div class="card lg:card-side bg-base-100 shadow-xl my-4">
+             <div class="w-full ld:w-1/2 "><img class="w-full h-full object-fill" src="${image_url}" alt="Album">
+             </div>
+       
+              <div class="card-body w=2/3 border-2 ">
+                <h2 class="card-title">${title? title:"NO Title Found"}</h2>
+                <p>${details.length > 200 ? details.slice(0,200) + '...' : details}</p>
+               <div class="card-actions flex justify-evenly">
+               <div class="w-12 h-12  rounded-full z-10 border-2 border-rose-600 overflow-hidden  " >
+               <img class="" src="${author.img}" alt="">
+               </div>
+               <div>
+                 <h5>${author.name?author.name:"No Author Found"}</h5>
+                 <p>${author.published_date?author.published_date.slice(0,11):"No Date"}</P>
+
+               </div>
+               <div>${total_view? total_view:00}</div>
+                 <button class="btn btn-primary">Listen</button>
+               </div>
+             </div>
       </div>
         `
         cardSection.appendChild(cardDiv);
-        console.log(data);
+        // console.log(data);
     })
    }
   ;
