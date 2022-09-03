1037a48de340bfd6aaaa2838f485f6fd381cddbb
Md. Nazmus Sakib <sakib036@gmail.com>
2022-09-03 22:08:02 +0600

    card-modal

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
 // menu section added 
 const menuList =async () =>{
+  try{
     const menuUrl=`https://openapi.programming-hero.com/api/news/categories`;
     const res=await fetch(menuUrl);
     const data=await res.json();
-    
     return (data.data.news_category);
+  }
+    catch(error){
+      alert(error);
+    }
+    
+   
 }
 
 const displayMenuList= async() =>{
@@ -16,18 +22,21 @@ const displayMenuList= async() =>{
         menuLi.innerHTML=`<a onclick="menuId('${menu.category_id}','${menu.category_name}')">${menu.category_name}</a>`;
         menuSection.appendChild(menuLi);
     });
-   
-
 }
 const menuId=(id, categoryName)=>{
     const spinner = document.getElementById("spinner");
     spinner.classList.remove("hidden");
     const categoryItem=async()=>{
-        const categoryUrl=`https://openapi.programming-hero.com/api/news/category/${id}`;
+        try{
+          const categoryUrl=`https://openapi.programming-hero.com/api/news/category/${id}`;
     const res=await fetch(categoryUrl);
     const categories=await res.json();
     
     displayCatagory(categories.data,categoryName);
+        }
+        catch(error){
+          alert(error);
+        }
    }
    const displayCatagory=(datas,name)=>{
     const categoryFound=document.getElementById('category-found');
@@ -46,7 +55,9 @@ const menuId=(id, categoryName)=>{
     }
     const valus=datas.sort((a, b) => b.total_view - a.total_view);
     valus.forEach(data=>{
-        const{image_url,title,details,author,rating,total_view}=data;
+      // console.log(data);
+        try{
+          const{image_url,title,details,author,rating,total_view,_id}=data;
         const cardDiv=document.createElement('div');
         cardDiv.innerHTML=`
         <div class="card lg:card-side bg-base-100 shadow-xl my-4">
@@ -67,28 +78,52 @@ const menuId=(id, categoryName)=>{
                </div>
                <div><i class="fa-solid fa-eye"></i>${total_view? total_view:00}</div>
                <div>
-               <label for="my-modal-6" class="btn modal-button" onclick="showDetails('${title}','${details? details:"No Data Found"}','${author.name?author.name:"No author Found"}')">open modal</label>
+               <label for="my-modal-6" class="btn modal-button" onclick="showDetails('${_id}')">open modal</label>
                </div>
              </div>
       </div>
         `
         cardSection.appendChild(cardDiv);
         spinner.classList.add("hidden")
-        console.log(data);
+        }
+        catch(error){
+          alert(error);
+        }
+        // console.log(data);
     })
    };
  
    categoryItem();
-}
+};
+
 displayMenuList();
 
-const showDetails=(title,details,author)=>{
-const modalBox=document.getElementById('modal-box');
-modalBox.innerHTML=`
-<h3 class="font-bold text-lg">${title}</h3>
-<p class="py-4">${details}</p>
-<p>${author}</p>
-`
- 
-  console.log(title);
+const showDetails=(id)=>{
+  // console.log(id);
+   const modalNewsId=async()=>{
+    try{
+      const newsUrl=`https://openapi.programming-hero.com/api/news/${id}`
+      const res=await fetch(newsUrl);
+    const news=await res.json();
+    const newsData=(news.data[0]);
+    const {title,details,author}=newsData;
+    const modalBox=document.getElementById('modal-box');
+    modalBox.innerHTML='';
+    const modalDiv=document.createElement('div');
+    modalDiv.innerHTML=`
+    <h3 class="font-bold text-lg">${title}</h3>
+            <p class="py-4">${details}</p>
+            <p>${author.name? author.name:"Author Not Found"}</P>
+            <div class="modal-action">
+                <label for="my-modal-6" class="btn">Yay!</label>
+              </div>
+    `
+    modalBox.appendChild(modalDiv);
+        }
+        catch(error){
+          alert(error);
+        }
+   }
+   modalNewsId();
 }
+
